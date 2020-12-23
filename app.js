ce8364cfc5848e81b81535677beaf5cb8e60780b
peterfchris <peter.f.chris@gmail.com>
2020-12-23 08:33:03 -0700

    Built in Constructors notes

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
 // Person constructor
-function Person(name, dob){
-  this.name = name
-  this.birthday = new Date(dob)
-  this.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime()
-    const ageDate = new Date(diff)
-    return Math.abs(ageDate.getUTCFullYear() - 1970)
-  }
+// function Person(name, dob){
+//   this.name = name
+//   this.birthday = new Date(dob)
+//   this.calculateAge = function(){
+//     const diff = Date.now() - this.birthday.getTime()
+//     const ageDate = new Date(diff)
+//     return Math.abs(ageDate.getUTCFullYear() - 1970)
+//   }
+// }
+
+// const peter = new Person('Peter', '6-26-1990')
+// console.log(peter.calculateAge())
+
+
+// Built in Constructors
+
+// Strings
+const name1 = 'Jeff'
+const name2 = new String('Jeff')
+
+// name2.foo = 'bar'
+// console.log(name2)
+
+console.log(typeof name2)
+
+if(name2 === 'Jeff'){
+  console.log('YES')
+} else {
+  console.log('NO')
 }
 
-const peter = new Person('Peter', '6-26-1990')
-console.log(peter.calculateAge())
+// Number
+const num1 = 5
+const num2 = new Number(5)
+
+// Boolean
+const bool1 = true
+const bool2 = new Boolean(true)
+
+// Function
+const getSum1 = function(x,y){
+  return x + y
+}
+
+const getSum2 = new Function('x','y', 'return 1 + 1')
+
+console.log(getSum2(1,1))
+
+// Object
+const john1 = {name: 'John'}
+const john2 = new Object({name: 'John'})
+console.log(john2)
+
+// Arrays
+const arr1 = [1,2,3,4]
+const arr2 = new Array(1,2,3,4)
+
+// Regular Expressions
+const re1 = /\w+/
+const re2 = new RegExp('\\w+')
+
+console.log(re2)
