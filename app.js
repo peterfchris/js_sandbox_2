6c474bef83bd43960efcd0a488202991faeac75c
peterfchris <peter.f.chris@gmail.com>
2021-01-05 13:26:16 -0700

    Prototype notes

--- app.js
+++ app.js
@@ -15,49 +15,93 @@
 
 // Built in Constructors
 
-// Strings
-const name1 = 'Jeff'
-const name2 = new String('Jeff')
+// // Strings
+// const name1 = 'Jeff'
+// const name2 = new String('Jeff')
 
-// name2.foo = 'bar'
-// console.log(name2)
+// // name2.foo = 'bar'
+// // console.log(name2)
 
-console.log(typeof name2)
+// console.log(typeof name2)
 
-if(name2 === 'Jeff'){
-  console.log('YES')
-} else {
-  console.log('NO')
+// if(name2 === 'Jeff'){
+//   console.log('YES')
+// } else {
+//   console.log('NO')
+// }
+
+// // Number
+// const num1 = 5
+// const num2 = new Number(5)
+
+// // Boolean
+// const bool1 = true
+// const bool2 = new Boolean(true)
+
+// // Function
+// const getSum1 = function(x,y){
+//   return x + y
+// }
+
+// const getSum2 = new Function('x','y', 'return 1 + 1')
+
+// console.log(getSum2(1,1))
+
+// // Object
+// const john1 = {name: 'John'}
+// const john2 = new Object({name: 'John'})
+// console.log(john2)
+
+// // Arrays
+// const arr1 = [1,2,3,4]
+// const arr2 = new Array(1,2,3,4)
+
+// // Regular Expressions
+// const re1 = /\w+/
+// const re2 = new RegExp('\\w+')
+
+// console.log(re2)
+
+
+// Prototypes Notes
+
+// Object.prototype
+// Person.prototype
+function Person(firstName, lastName, dob){
+  this.firstName = firstName
+  this.lastName = lastName
+  this.birthday = new Date(dob)
 }
 
-// Number
-const num1 = 5
-const num2 = new Number(5)
-
-// Boolean
-const bool1 = true
-const bool2 = new Boolean(true)
-
-// Function
-const getSum1 = function(x,y){
-  return x + y
+// Calculate age
+Person.prototype.calculateAge = function(){
+  const diff = Date.now() - this.birthday.getTime()
+  const ageDate = new Date(diff)
+  return Math.abs(ageDate.getUTCFullYear() - 1970)
 }
 
-const getSum2 = new Function('x','y', 'return 1 + 1')
+// Get full name
+Person.prototype.getFullName = function(){
+  return `${this.firstName} ${this.lastName}`
+}
 
-console.log(getSum2(1,1))
+// Gets Married
+Person.prototype.getsMarried = function(newLastName){
+  this.lastName = newLastName
+}
 
-// Object
-const john1 = {name: 'John'}
-const john2 = new Object({name: 'John'})
-console.log(john2)
+const john = new Person('John', 'Doe', '8-12-90')
+const mary = new Person('Mary', 'Johnson', 'March 20 1978')
 
-// Arrays
-const arr1 = [1,2,3,4]
-const arr2 = new Array(1,2,3,4)
+console.log(mary)
 
-// Regular Expressions
-const re1 = /\w+/
-const re2 = new RegExp('\\w+')
+console.log(john.calculateAge())
 
-console.log(re2)
+console.log(mary.getFullName())
+
+mary.getsMarried('Smith')
+
+console.log(mary.getFullName())
+
+console.log(mary.hasOwnProperty('firstName'))
+console.log(mary.hasOwnProperty('getFullName'))
