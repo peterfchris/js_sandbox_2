7434e6f27b6335dcd9fb71b1e242b5c618bb6ffe
peterfchris <peter.f.chris@gmail.com>
2021-01-06 13:56:33 -0700

    prototype inheritance notes

--- app.js
+++ app.js
@@ -105,3 +105,45 @@
 
 // console.log(mary.hasOwnProperty('firstName'))
 // console.log(mary.hasOwnProperty('getFullName'))
+
+
+// PROTOTYPAL Inheritance
+
+// Person constructor
+function Person(firstName, lastName) {
+  this.firstName = firstName
+  this.lastName = lastName
+}
+
+// Greeting
+Person.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName}`
+}
+
+const person1 = new Person('John', 'Doe')
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership){
+  Person.call(this, firstName, lastName)
+
+  this.phone = phone
+  this.membership = membership
+}
+
+// Inherit the person prototype methods
+Customer.prototype = Object.create(Person.prototype)
+
+// Make customer .prototype return Customer()
+Customer.prototype.constructor = Customer
+
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard')
+
+console.log(customer1)
+
+// Customer greeting
+Customer.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`
+}
+
+console.log(customer1.greeting())
