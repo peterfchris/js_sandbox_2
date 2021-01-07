f1deda44f983154feae21cbb3fe9d76fabaec3e7
peterfchris <peter.f.chris@gmail.com>
2021-01-07 16:07:06 -0700

    Object.create notes part 2

--- app.js
+++ app.js
@@ -149,30 +149,30 @@
 // console.log(customer1.greeting())
 
 
-// Using object.create
-const personPrototypes = {
-  greeting: function(){
-    return `Hello there ${this.firstName} ${this.lastName}`
-  },
-  getsMarried: function(newLastName){
-    this.lastName = newLastName
-  }
-}
+// // Using object.create
+// const personPrototypes = {
+//   greeting: function(){
+//     return `Hello there ${this.firstName} ${this.lastName}`
+//   },
+//   getsMarried: function(newLastName){
+//     this.lastName = newLastName
+//   }
+// }
 
-const mary = Object.create(personPrototypes)
-mary.firstName = 'Mary'
-mary.lastName = 'Williams'
-mary.age = 30
+// const mary = Object.create(personPrototypes)
+// mary.firstName = 'Mary'
+// mary.lastName = 'Williams'
+// mary.age = 30
 
-mary.getsMarried('Thompson')
+// mary.getsMarried('Thompson')
 
-console.log(mary.greeting())
+// console.log(mary.greeting())
 
-const brad = Object.create(personPrototypes, {
-  firstName: {value: 'brad'},
-  lastName: {value: 'perkins'},
-  age: {value: 36}
-})
+// const brad = Object.create(personPrototypes, {
+//   firstName: {value: 'brad'},
+//   lastName: {value: 'perkins'},
+//   age: {value: 36}
+// })
 
-console.log(brad)
-console.log(brad.greeting())
+// console.log(brad)
+// console.log(brad.greeting())
