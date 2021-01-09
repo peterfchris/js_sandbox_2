1a83c3e11017efbddf5ca52c5882167c3327f567
peterfchris <peter.f.chris@gmail.com>
2021-01-09 11:38:22 -0700

    Sub class Notes

--- app.js
+++ app.js
@@ -179,35 +179,67 @@
 
 // Class Notes
 
+// class Person {
+//   constructor(firstName, lastName, dob){
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.birthday = new Date(dob)
+//   }
+
+//   greeting(){
+//     return `Hello there ${this.firstName} ${this.lastName}`
+//   }
+
+//   calculateAge(){
+//     const diff = Date.now() - this.birthday.getTime()
+//     const ageDate = new Date(diff)
+//     return Math.abs(ageDate.getUTCFullYear() - 1970)
+//   }
+
+//   getsMarried(newLastName){
+//     this.lastName = newLastName
+//   }
+//   static addNumbers(x, y){
+//     return x + y
+//   }
+// }
+
+// const mary = new Person('Mary', 'Williams', '11-13-1980')
+
+// mary.getsMarried('Thompson')
+
+// console.log(mary)
+
+// console.log(Person.addNumbers(1, 2))
+
+// Sub Class Notes
+
 class Person {
-  constructor(firstName, lastName, dob){
+  constructor(firstName, lastName){
     this.firstName = firstName
     this.lastName = lastName
-    this.birthday = new Date(dob)
   }
 
   greeting(){
-    return `Hello there ${this.firstName} ${this.lastName}`
-  }
-
-  calculateAge(){
-    const diff = Date.now() - this.birthday.getTime()
-    const ageDate = new Date(diff)
-    return Math.abs(ageDate.getUTCFullYear() - 1970)
-  }
-
-  getsMarried(newLastName){
-    this.lastName = newLastName
-  }
-  static addNumbers(x, y){
-    return x + y
+    return  `Hello there ${this.firstName} ${this.lastName}`
   }
 }
 
-const mary = new Person('Mary', 'Williams', '11-13-1980')
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership){
+    super(firstName, lastName)
 
-mary.getsMarried('Thompson')
+    this.phone = phone
+    this.membership = membership
+  }
 
-console.log(mary)
+  static getMembershipCost(){
+    return 500
+  }
+}
 
-console.log(Person.addNumbers(1, 2))
+const john = new Customer('john', 'doe', '555-555-5555', 'Standard')
+
+console.log(john.greeting())
+
+console.log(Customer.getMembershipCost())
